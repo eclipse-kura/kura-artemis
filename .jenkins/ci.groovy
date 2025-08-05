b532d9e971f7a309cb4d0d434f38c9de5ebaaf15
Marcello Rinaldo Martina <39562568+marcellorinaldo@users.noreply.github.com>
2025-08-05 12:05:32 +0200

    feat: added Artemis bundles and resources (#2)
    * feat: added Artemis bundles and resources
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    * chore: added PR template
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    * chore: use embed dependency
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    * build: added kura-artemis-bom
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    * build: bump commons-beanutils to 1.11.0
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    * ci: migrate to shared CI pipeline (#5)
    
    * build: fix versions
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    * build(distrib): refactor build to not rely on properties
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    * review suggestion: use a single gitignore
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    * review suggestion: missing .0 in version
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    
    ---------
    
    Signed-off-by: Marcello Rinaldo Martina <martina.marcello.rinaldo@outlook.com>
    Co-authored-by: Mattia Dal Ben <mattdibi@users.noreply.github.com>

---
+++ .jenkins/ci.groovy
@@ -0,0 +1,13 @@
+@Library('add-ons-shared-libs@develop') _
+
+node {
+    continuousIntegrationPipeline(
+        buildType: "deploy",
+        sonar: [
+            enable: true,
+            projectKey: "eclipse-kura_kura-artemis",
+            tokenId: "sonarcloud-token-kura-artemis",
+            exclusions: "tests/**/*.java"
+        ],
+    )
+}
