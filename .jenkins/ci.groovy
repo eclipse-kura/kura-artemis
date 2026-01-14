10f8cb467bc669622f2ccf762d190f95c4e0981a
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2026-01-14 11:38:53 +0100

    Update jenkins groovy file with new config
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- .jenkins/ci.groovy
+++ .jenkins/ci.groovy
@@ -7,8 +7,8 @@ node {
             enable: true,
             projectKey: "eclipse-kura_kura-artemis",
             tokenId: "sonarcloud-token-kura-artemis",
-            exclusions: "*.xml",
-            testExclusions: "**/*.java,*.xml"
+            exclusions: "**/*.xml,**/*.yml",
+            testExclusions: "**/*"
         ],
     )
 }
